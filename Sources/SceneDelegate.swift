48ce080006c949157c64b4284bc5b2bdc93604f7
Dmitriy_Mk <Dmitriy.mkrtumov@gmail.com>
2025-03-01 20:49:41 +0400

    Delegates modif

--- Sources/SceneDelegate.swift
+++ Sources/SceneDelegate.swift
@@ -1,7 +1,10 @@
 import UIKit
 import AuthModule
 
+
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
